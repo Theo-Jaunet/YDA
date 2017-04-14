8323546f061d0b143f30175f10b884ba2c9daedb
Marc Portet <marcsuitgundamseed@hotmail.fr>
2017-04-14 22:13:54 +0200

    Adding Fungus plugin, this will allow us to add Visual Novel scripting

---
+++ Assets/Scripts/UI/Talk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Talk : MonoBehaviour {
+
+	// text area of person 1
+	public Text BText1;
+
+	// text area of person 2
+	public Text BText2;
+
+	// Name of P1
+	public Text SText1;
+
+	// Name of P2
+	public Text SText2;
+
+	public int idChat = 1;
+
+
+	
+}
