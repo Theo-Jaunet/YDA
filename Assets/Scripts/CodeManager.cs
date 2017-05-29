a3e53b18d3e684c651fa5afb84ae8b74742ceecf
Marc Portet <marcsuitgundamseed@hotmail.fr>
2017-05-29 17:47:49 +0200

    We can now detect the correct label in the slot

--- Assets/Scripts/CodeManager.cs
+++ Assets/Scripts/CodeManager.cs
@@ -20,25 +20,60 @@ public class CodeManager : MonoBehaviour {
 
 	public void setActions()
 	{
+		// GO = Game Objects
 		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Case");
-		int idArray = 0;
 		actionCases = new string[gameObjects.Length];
+		Queue<GameObject> queueTempGO = new Queue<GameObject>();
+		int indexArrayGO = 0;
+
+		// creating a temporary queue to sort easily the game objects by name
+		for (int i = 0; i < gameObjects.Length; i++)
+		{
+			queueTempGO.Enqueue(gameObjects[i]);
+		}
+
+		bool arraySorted = false;
+
+		int sizeIndex = gameObjects.Length;
+
+		// loop to sort the GO
+		while (!arraySorted || indexArrayGO > sizeIndex)
+		{
+			string stringGO = queueTempGO.Peek().ToString();
+			string indexGO = stringGO.Substring(4, 1);
+
+			if (indexArrayGO.ToString() == indexGO.ToString())
+			{
+				// remove the object in the queue and returns it to the temporary array
+				gameObjects[indexArrayGO] = queueTempGO.Dequeue();
+			}
+
+			if (indexArrayGO >= sizeIndex)
+			{
+				indexArrayGO = 0;
+			}
+
+			// we can end the loop now
+			if (queueTempGO.Count == 0)
+			{
+				arraySorted = true;
+			}
+
+			indexArrayGO++;
+		}
+
+		print("ending of the loop of ze death");
+
 
 		for (int i = 0; i < gameObjects.Length; i++)
 		{
-			print(gameObjects[i].ToString());
-			if (gameObjects[i].transform.GetChildCount() > 0)
-			{
-				actionCases[idArray] = gameObjects[i].transform.GetChild(0).ToString();
-				idArray++;		
-			}
+			actionCases[i] = gameObjects[i].ToString() + gameObjects[i].transform.GetChild(0).ToString();
 		}
 
-		for(int i=0; i < actionCases.Length; i++)
+		for (int i = 0; i < actionCases.Length; i++)
 		{
 			print(actionCases[i]);
 		}
 	}
-
 
 }
